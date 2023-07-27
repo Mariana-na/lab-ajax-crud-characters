bcd4767df3ac981a68a1a0dfcc691cc532c30ddd
Mariana Ribeiro de Carvalho <mariana.rcarvalho.992gmail.com>
2023-07-27 16:19:06 +0100

    Fetch all

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,52 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL = axios.create ({
+      baseURL: "http://localhost:3000/characters" 
+    });
   }
 
-  getFullList () {
+  async getFullList () {
+
+      const response = await axios.get ('http://localhost:3000/characters');
+
+      if (response.status === 200) {
+        console.log("full list")
+      } else {
+        console.log("wrong")
+      }
+
+      return response
 
   }
 
-  getOneRegister () {
+  getOneRegister (id) {
+    axios
+    .get("http://localhost:3000/characters/:id")
+    .then ((response) => response.json())
+    .then ((data) => {
+      console.log(data)
+    })
+    .catch ((error) => console.log(error));
+  }
+
+  createOneRegister (characterInfo) {
+    return this.BASE_URL.post('', characterInfo)
 
   }
 
-  createOneRegister () {
+  updateOneRegister (characterId, characterInfo) {
+    return this.BASE_URL.put('/:id', characterInfo)
 
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  async deleteOneRegister (characterId) {
+    const response = await axios.delete ('http://localhost:3000/characters/:id');
+    if (response.status === 200) {
+      console.log("Successfuly deleted")
+    } else {
+      console.log("id doesn't exist")
+    }
+    return response.data
 
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,7 +1,22 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('http://localhost:3000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', async () => {
+    try {
+      const dataStore =  await charactersAPI.getFullList()
+      const charactersContainer = document.querySelector('.characters-container');
+      charactersContainer.innerHTML = '';
+
+      console.log(dataStore)
+
+      dataStore.data.forEach ((character) => {
+        const card = characterCard(character);
+        charactersContainer.appendChild(card);
+
+      })
+    } catch (error) {
+      console.log(error)
+    }
 
   });
 
@@ -20,4 +35,31 @@ window.addEventListener('load', () => {
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
 
   });
+
+
+  function characterCard(character) {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('character-info');
+
+    const nameElement = document.createElement('div');
+    nameElement.textContent = `Name: ${character.name}`;
+
+    const occupationElement = document.createElement('div');
+    occupationElement.textContent = `Occupation: ${character.occupation}`;
+
+    const weaponElement = document.createElement('div');
+    weaponElement.textContent = `Weapon: ${character.weapon}`;
+
+    const cartoonElement = document.createElement('div');
+    cartoonElement.textContent = `Is a Cartoon: ${character.cartoon ? 'Yes' : 'No'}`;
+
+    cardContainer.appendChild(nameElement);
+    cardContainer.appendChild(occupationElement);
+    cardContainer.appendChild(weaponElement);
+    cardContainer.appendChild(cartoonElement);
+
+    return cardContainer;
+  }
+
+
 });
